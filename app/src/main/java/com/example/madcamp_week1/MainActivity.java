0f80c19a2dd9bbe1581cdee0f55f1bc2b4ad487e
alexhonggi <alexhonggi@gmail.com>
2021-07-01 17:24:10 +0900

    3 tabs with swipe function, using ViewPager

--- app/src/main/java/com/example/madcamp_week1/MainActivity.java
+++ app/src/main/java/com/example/madcamp_week1/MainActivity.java
@@ -1,55 +1,74 @@
 package com.example.madcamp_week1;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
 
-    TabLayout tabs;
-    PhoneFragment fragmentPhone;
-    ImageFragment fragmentImage;
-    FreeFragment fragmentFree;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentPhone = new PhoneFragment();
-        fragmentImage = new ImageFragment();
-        fragmentFree = new FreeFragment();
+        ViewPager pager = findViewById(R.id.pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.container, fragmentPhone).commit();
 
-        tabs = findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setText("Phone"));
-        tabs.addTab(tabs.newTab().setText("Image"));
-        tabs.addTab(tabs.newTab().setText("Free"));
+        pager.setOffscreenPageLimit(3); //현재 페이지의 양쪽에 보유해야하는 페이지 수를 설정 (상황에 맞게 사용하시면 됩니다.)
+        tabLayout.setupWithViewPager(pager); //텝레이아웃과 뷰페이저를 연결
+        pager.setAdapter(new PageAdapter(getSupportFragmentManager(),this)); //뷰페이저 어뎁터 설정 연결
 
-        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                Fragment selected = null;
-                if(position == 0)
-                    selected = fragmentPhone;
-                else if(position == 1)
-                    selected = fragmentImage;
-                else if(position == 2)
-                    selected = fragmentFree;
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
-            }
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
+
     }
+    static class PageAdapter extends FragmentStatePagerAdapter { //뷰 페이저 어뎁터
 
+
+        PageAdapter(FragmentManager fm, Context context) {
+            super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        }
+
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) { //프래그먼트 사용 포지션 설정 0 이 첫탭
+                return new PhoneFragment();
+            } else if (position == 1){
+                return new ImageFragment();
+            } else {
+                return new FreeFragment();
+            }
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) { //텝 레이아웃의 타이틀 설정
+                return "Phone";
+            } else if (position == 1){
+                return "Image";
+            } else {
+                return "Free";
+            }
+        }
+    }
 }
