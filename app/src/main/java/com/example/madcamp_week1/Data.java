89adae12b8a03358d0b19dc342c26641c7186e94
윤영훈 <wodlxosxos73@gmail.com>
2021-07-01 13:33:25 +0900

    set Data, RecyclerViewAdapter

---
+++ app/src/main/java/com/example/madcamp_week1/Data.java
@@ -0,0 +1,32 @@
+package com.example.madcamp_week1;
+
+public class Data {
+
+    private String user_name;
+    private String user_number;
+    //private int resId;
+
+    public String getName() {
+        return user_name;
+    }
+
+    public void setName(String name) {
+        this.user_name = name;
+    }
+
+    public String getNumber() {
+        return user_number;
+    }
+
+    public void setNumber(String number) {
+        this.user_number = number;
+    }
+
+    /*public int getResId() {
+        return resId;
+    }
+
+    public void setResId(int resId) {
+        this.resId = resId;
+    }*/
+}
